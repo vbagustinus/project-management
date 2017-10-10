443a93b9ee9749e833c55c1031f800fd42f31331
vbagustinus <vbagustinus@gmail.com>
2017-10-10 16:27:47 +0700

    Done Project

---
+++ helper/randomNumber.js
@@ -0,0 +1,15 @@
+var randomAlphanumeric = function(length) {
+    var str = '';
+    var chars ='0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split(
+        '');
+    var charsLen = chars.length;
+    if (!length) {
+      length = ~~(Math.random() * charsLen);
+    }
+    for (var i = 0; i < length; i++) {
+      str += chars[~~(Math.random() * charsLen)];
+    }
+    return str;
+  };
+  module.exports = randomAlphanumeric;
+  
